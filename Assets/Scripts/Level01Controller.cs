70bb596ed753c8a25a8dfec80b4c1496075e2b0d
Hitarth Mistry <hitarthmistry42@gmail.com>
2020-10-05 18:00:35 -0500

    Full Game Navigation Functionality

---
+++ Assets/Scripts/Level01Controller.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Level01Controller : MonoBehaviour
+{
+    [SerializeField] Text _currentScoreTextView;
+    [SerializeField] GameObject PauseMenu;
+
+    int _currentScore;
+    private void Update()
+    {
+        // Increase Score
+        //TODO replace with real implementation later
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            IncreaseScore(5);
+        }
+        // Exit Level
+        //TODO bring up popup menu for navigation
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            PauseMenu.SetActive(true);
+        }
+    }
+    public void ExitLevel()
+    {
+        // compare score to high score
+        int highScore = PlayerPrefs.GetInt("HighScore");
+        if (_currentScore > highScore)
+        {
+            // save current score as new high score
+            PlayerPrefs.SetInt("HighScore", _currentScore);
+            Debug.Log("New high score: " + _currentScore);
+        }
+
+        //disable pause menu
+        PauseMenu.SetActive(false);
+        // load new level
+        SceneManager.LoadScene("MainMenu");
+    }
+    public void IncreaseScore(int scoreIncrease)
+    {
+        // increase score
+        _currentScore += scoreIncrease;
+        // update score display, so we can see the new score
+        _currentScoreTextView.text =
+            "Score: " + _currentScore.ToString();
+    }
+}
---
+++ Assets/Scripts/MainMenuController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainMenuController : MonoBehaviour
+{
+    [SerializeField] AudioClip _startingSong;
+    [SerializeField] Text _highScoreTextView;
+
+    // Start is called before hte first frame update
+    void Start()
+    {
+        // load high score display
+        int highScore = PlayerPrefs.GetInt("HighScore");
+        _highScoreTextView.text = highScore.ToString();
+        if(_startingSong != null)
+        {
+            AudioManager.Instance.PlaySong(_startingSong);
+        }
+    }
+    public void ResetData()
+    {
+        PlayerPrefs.SetInt("HighScore", 0);
+        Debug.Log("Reset High score");
+        int highScore = PlayerPrefs.GetInt("HighScore");
+        _highScoreTextView.text = highScore.ToString();
+    }
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
