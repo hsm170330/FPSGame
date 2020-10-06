1ece7036e7139ce5a0b94d5f6bc1c08a25593c63
Hitarth Mistry <hitarthmistry42@gmail.com>
2020-10-05 23:02:41 -0500

    Added Gun Audio
    gun will fire randomly selected audio from 3 different but similar gun shot audio

--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -32,4 +32,19 @@ public class AudioManager : MonoBehaviour
         _audioSource.clip = clip;
         _audioSource.Play();
     }
+    public static AudioSource PlayClip2D(AudioClip clip, float volume)
+    {
+        // create our new AudioSource
+        GameObject audioObject = new GameObject("2DAudio");
+        AudioSource audioSource = audioObject.AddComponent<AudioSource>();
+        // configure it to be 2D
+        audioSource.clip = clip;
+        audioSource.volume = volume;
+
+        audioSource.Play();
+        // destroy when it's done
+        Object.Destroy(audioObject, clip.length);
+        // return it
+        return audioSource;
+    }
 }
--- Assets/Scripts/GunScript.cs
+++ Assets/Scripts/GunScript.cs
@@ -6,6 +6,10 @@ public class GunScript : MonoBehaviour
     public float damage = 10f;
     public float range = 100f;
 
+    [SerializeField] AudioClip gun01 = null;
+    [SerializeField] AudioClip gun02 = null;
+    [SerializeField] AudioClip gun03 = null;
+
     public ParticleSystem muzzleFlash;
     public Camera fpsCam;
     // Update is called once per frame
@@ -25,5 +29,18 @@ public class GunScript : MonoBehaviour
             Debug.Log(hit.transform.name);
         }
         muzzleFlash.Play();
+        int s = Random.Range(0, 3);
+        if (s == 0)
+        {
+            AudioManager.PlayClip2D(gun01, 1);
+        }
+        else if (s == 1)
+        {
+            AudioManager.PlayClip2D(gun02, 1);
+        }
+        else if (s == 2)
+        {
+            AudioManager.PlayClip2D(gun03, 1);
+        }
     }
 }
